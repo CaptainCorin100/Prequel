2ba0f8f13accf70600e7f3a8ed83033be25ab55d
aski6 <aski6>
2017-08-08 21:36:05 +0100

    Started adding database connection code

--- budget-sheets.py
+++ budget-sheets.py
@@ -1,11 +1,14 @@
 from flask import Flask, session, render_template, request, redirect, escape
+import pymysql.cursors
 
 app = Flask(__name__)
+db = pymysql.connect(host="localhost", user="budget-sheets", passwd="sheets-budget", db="budget-sheets", cursorclass=pymysql.cursors.DictCursor)
+cursor = db.cursor()
 
 @app.route("/")
 def index():
     if "username" in session:
-        return render_template("home.html", stylesheet=url_for("budget-sheets/static", filename="style.css"))
+        return render_template("home.html")
     else:
         return render_template("home.html")
 
@@ -13,6 +16,7 @@ def index():
 def login():
     if request.method == "POST":
         app.logger.info("User " + request.form['username'] + " requested login")
+
         session['username'] = request.form['username']
         return redirect(url_for('index'))
     else:
@@ -26,10 +30,11 @@ def logout():
 @app.route("/signup/", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        
+        return redirect(url_for("login"))
     return redirect(url_for("index"))
 
-app.secret_key = "b'*_dx$@\xbe\x91v\x1d\xd8M\xaeC\xee\xe4\x90J\x15\xc4"\x16(\x13'"
+app.secret_key = "b@*_dx$'\xbe\x91v\x1d\xd8M\xaeC\xee\xe4\x90J\x15\xc4%\x16(\x13'"
 
-if(__name__ == "__main__"):
-    app.run(host="0.0.0.0")
+if (__name__ == "__main__"):
+    if (db != None):
+        app.run(host="0.0.0.0")
