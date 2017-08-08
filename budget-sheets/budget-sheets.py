2169373ba34e60f27cd18de2498df68e3e286680
aski6 <aski6@users.noreply.github.com>
2017-08-08 11:53:08 +0100

    Starting to add logins

--- budget-sheets/budget-sheets.py
+++ budget-sheets/budget-sheets.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, session, render_template, request, redirect, escape
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():  #define what happens when the user asks to load home
-    if (request.method == "POST"): #check if the user sent info using post
-        name = request.form["username"] #get the username sent when loading the pageh
-        logged_in = True
-        if(logged_in):
-            return render_template("home.html", description="Budget software", username=name) #show the user the page with their username
+@app.route("/")
+def index():
+    if "username" in session:
+        return render_template("home.html")
     else:
-        return render_template("home.html", description="Budget software", username="")
+        return render_template("home.html")
+
+@app.route("/login/"):
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        return render_template("login.html")
 
 if(__name__ == "__main__"):
-    app.run()
-user.n
+    app.run(host="0.0.0.0")
