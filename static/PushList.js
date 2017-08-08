46fc4e73d219c0b6c40c4c94daaa8f0b2a0e6acf
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-08 15:31:46 +0100

    Basic JS functionality

--- static/PushList.js
+++ static/PushList.js
@@ -1,3 +1,5 @@
 function push () {
-	document.getElementByID ("stats_form_expense").innerHTML += <div><label>Purchase Type:</label><select><option value="food">Food</option><option value="appliances">Appliances</option><option value="luxuries">Luxuries</option></select><label >Purchase Amount:</label><input type="text" name="Amount:" id="stats_form_netpay"></div>;
+	var div = document.createElement ("div"); 
+	div.innerHTML = "<div>\n"+"<label>Purchase Type:</label>\n"+"<select>\n"+"<option value="food">Food</option>\n"+"<option value="appliances">Appliances</option>\n"+"<option value="luxuries">Luxuries</option>\n"+"</select>\n"+"<label>Purchase Amount:</label>\n"+"<input type="text" name="Amount:" id="stats_form_netpay">\n"+"</div>";
+	document.GetElementById("stats_form_expense").appendChild (div);
 }
