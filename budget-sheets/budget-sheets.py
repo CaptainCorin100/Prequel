37096b58fd87b873fb88c999857cc7c774bf161d
aski6 <aski6@users.noreply.github.com>
2017-08-08 12:21:51 +0100

    maybe... maybe we can have css now.

---
+++ budget-sheets/budget-sheets.py
@@ -0,0 +1,28 @@
+from flask import Flask, session, render_template, request, redirect, escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    if "username" in session:
+        return render_template("home.html")
+    else:
+        return render_template("home.html")
+
+@app.route("/login/")
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+app.secret_key = "This key will be better when it is actually secret, and loaded from a database."
+
+if(__name__ == "__main__"):
+    app.run(host="0.0.0.0")
