190c2efbfea5ef5c26be57522c7c50d37fb77841
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-08 15:00:17 +0100

    Javascript stuff

--- static/PushList.js
+++ static/PushList.js
@@ -1,3 +1,3 @@
 function push () {
-	document.getElementByID ("stats_form_expense").innerHTML += <label for="stats_form_netpay">Net Pay:</label><input type="text" name="Amount:" id="stats_form_netpay">;
+	document.getElementByID ("stats_form_expense").innerHTML += <div><label>Purchase Type:</label><select><option value="food">Food</option><option value="appliances">Appliances</option><option value="luxuries">Luxuries</option></select><label >Purchase Amount:</label><input type="text" name="Amount:" id="stats_form_netpay"></div>;
 }
