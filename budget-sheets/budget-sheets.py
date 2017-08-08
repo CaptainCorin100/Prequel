4d6d0375fefb63b91e68b7e685f00278bae31297
aski6 <aski6@users.noreply.github.com>
2017-08-08 10:53:16 +0100

    Working on some extra code for home.html

--- budget-sheets/budget-sheets.py
+++ budget-sheets/budget-sheets.py
@@ -1,10 +1,16 @@
-from flask import Flask, render-template, request, redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if (request.method == "POST"):
+        name = request.form["username"]
+        logged_in = True
+        if(logged_in):
+            return render_template("home.html", description="Budget software", username=name)
+    else:
+        return render_template("home.html", description="Budget software", username="")
 
 if(__name__ == "__main__"):
     app.run()
