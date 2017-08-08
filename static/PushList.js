4a190f3a8a620f365da94f6587699c5afbf304c7
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-08 19:43:27 +0100

    Added list removing

--- static/PushList.js
+++ static/PushList.js
@@ -1,5 +1,13 @@
+var expense = document.getElementById("stats_form_expense");
 function push () {
-	var div = document.createElement ("div"); 
-	div.innerHTML = '<div>\n'+'<label>Purchase Type:</label>\n'+'<select>\n'+'<option value="food">Food</option>\n'+'<option value="appliances">Appliances</option>\n'+'<option value="luxuries">Luxuries</option>\n'+'</select>\n'+'<label>Purchase Amount:</label>\n'+'<input type="text" name="Amount:" id="stats_form_netpay">\n'+'</div>';
-	document.getElementById("stats_form_expense").appendChild (div);
+	if (document.getElementById("stats_form_expense").children.length <= 6) {
+		var div = document.createElement ("div"); 
+		div.innerHTML = '<div>\n'+'<label>Purchase Type:</label>\n'+'<select>\n'+'<option value="food">Food</option>\n'+'<option value="appliances">Appliances</option>\n'+'<option value="luxuries">Luxuries</option>\n'+'</select>\n'+'<label>Purchase Amount:</label>\n'+'<input type="text" name="Amount:" id="stats_form_netpay">\n'+'</div>';
+		document.getElementById("stats_form_expense").appendChild (div);
+	}
+}
+function remove () {
+	if (document.getElementById("stats_form_expense").children.length > 1) {
+		document.getElementById("stats_form_expense").removeChild (document.getElementById("stats_form_expense").children[document.getElementById("stats_form_expense").children.length - 1]);
+	}
 }
