09e610a49c5261801e3e8473be7723617d8a1aa9
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-08 14:46:26 +0100

    fix

---
+++ static/PushList.js
@@ -0,0 +1,3 @@
+function push () {
+	document.getElementByID ("stats_form_expense").innerHTML += <label for="stats_form_netpay">Net Pay:</label><input type="text" name="Amount:" id="stats_form_netpay">;
+}
