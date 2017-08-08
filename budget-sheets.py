1ae6ae4c9e42d94bfbecef1a77492f007ff1e5c1
aski6 <aski6@users.noreply.github.com>
2017-08-08 20:42:32 +0100

    working on setting up user logins and sessions.

--- budget-sheets.py
+++ budget-sheets.py
@@ -9,9 +9,10 @@ def index():
     else:
         return render_template("home.html")
 
-@app.route("/login/")
+@app.route("/login/", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
+        app.logger.info("User " + request.form['username'] + " requested login")
         session['username'] = request.form['username']
         return redirect(url_for('index'))
     else:
@@ -22,7 +23,13 @@ def logout():
     session.pop("username", None)
     return redirect(url_for("index"))
 
-app.secret_key = "This key will be better when it is actually secret, and loaded from a database."
+@app.route("/signup/", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        
+    return redirect(url_for("index"))
+
+app.secret_key = "b'*_dx$@\xbe\x91v\x1d\xd8M\xaeC\xee\xe4\x90J\x15\xc4"\x16(\x13'"
 
 if(__name__ == "__main__"):
     app.run(host="0.0.0.0")
