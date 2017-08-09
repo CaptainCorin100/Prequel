0c09cf4cdcda7ea0000dcf41d79eb2d82dec33df
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-09 16:04:27 +0100

    Some JS

--- static/FormVerify.js
+++ static/FormVerify.js
@@ -1,14 +1,16 @@
-var form = document.getElementById ("home_form");
-console.log (form);
-var name = form.children[2];
-//var mail = form.children[5];
-var pass = form.children[8];
-var pass2 = form.children[11];
+window.onLoad = function () {
+	var form = document.getElementById ("home_body_left");
+	console.log (form);
+	var name = form.children[2];
+	//var mail = form.children[5];
+	var pass = form.children[8];
+	var pass2 = form.children[11];
 
-name.onchange = validateName ();
-//mail.onchange = validateMail ();
-pass.onchange = validatePassword ();
-pass2.onchange = validatePassword ();
+	name.onchange = validateName ();
+	//mail.onchange = validateMail ();
+	pass.onchange = validatePassword ();
+	pass2.onchange = validatePassword ();
+}
 
 function validateName () {
 	if (name.value.length < 6) {
