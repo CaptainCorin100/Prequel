e0c242347a2dc59d67746011e8a2c6fff4eee697
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-09 11:46:52 +0100

    Added for with JS

---
+++ static/DrawCharts.js
@@ -0,0 +1,6 @@
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart () {
+	//var data = new google.visualisation
+}
--- static/PushList.js
+++ static/PushList.js
@@ -2,7 +2,19 @@ var expense = document.getElementById("stats_form_expense");
 function push () {
 	if (document.getElementById("stats_form_expense").children.length <= 6) {
 		var div = document.createElement ("div"); 
-		div.innerHTML = '<div>\n'+'<label>Purchase Type:</label>\n'+'<select>\n'+'<option value="food">Food</option>\n'+'<option value="appliances">Appliances</option>\n'+'<option value="luxuries">Luxuries</option>\n'+'</select>\n'+'<label>Purchase Amount:</label>\n'+'<input type="text" name="Amount:" id="stats_form_netpay">\n'+'</div>';
+		div.innerHTML = '<div id="stats_form_expense_row0">\n' + 
+						'<label>Expense Category:</label>\n' +
+						'<select>\n' +
+							'<option value="food">Food</option>\n' +
+							'<option value="appliances">Appliances</option>\n' +
+							'<option value="luxuries">Luxuries</option>\n' +
+							'<option value="other">Other</option>\n' +
+						'</select>\n' +
+						'<label>Expense Name:</label>\n' +
+						'<input type="text" name="name" id="stats_form_netpay">\n' +
+						'<label>Cost:</label>\n'+
+						'<input type="text" name="cost" id="stats_form_netpay">\n'+
+					'</div>';
 		div.id = "stats_form_expense_row" + ((document.getElementById("stats_form_expense").children.length-1).toString());
 		document.getElementById("stats_form_expense").appendChild (div);
 		console.log (div.id);
