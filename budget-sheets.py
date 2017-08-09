fd2c694a7747eecc7f25bbe34d8801f0af24874f
aski6 <aski6@users.noreply.github.com>
2017-08-09 13:57:21 +0100

    Fixed error while adding user accounts

--- budget-sheets.py
+++ budget-sheets.py
@@ -21,7 +21,7 @@ def create_user_account(username, email, password):
     password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     sql = 'INSERT INTO `users` (`username`, `email`, `password`) VALUES ("{}", "{}", "{}")'
     print(sql.format(username, email, password_hash.decode("utf-8")))
-    cursor.execute(sql.format(username, email, password_hash))
+    cursor.execute(sql.format(username, email, password_hash.decode("utf-8")))
     db.commit()
     return True
 
