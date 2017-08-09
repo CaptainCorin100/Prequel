0c0058a48c8383b65f8050818112a8aa1b5c2357
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-09 15:14:42 +0100

    Bug ridden JS

--- static/FormVerify.js
+++ static/FormVerify.js
@@ -1,4 +1,25 @@
 var form = document.getElementById ("home_form");
-form.noValidate = true;
+var name = form.children[2];
+//var mail = form.children[5];
+var pass = form.children[8];
+var pass2 = form.children[11];
 
-form.onSubmit = validateForm;
+name.onchange = validateName ();
+//mail.onchange = validateMail ();
+pass.onchange = validatePassword ();
+pass2.onchange = validatePassword ();
+
+function validateName () {
+	if (name.value.length < 6) {
+		name.setCustomValidity ("Name is too short");
+	} else {
+		name.setCustomValidity ("");
+	}
+} 
+function validatePassword () {
+	if (pass.value != pass2.value) {
+		pass2.setCustomValidity ("Passwords do not match");
+	} else {
+		
+	}
+}
