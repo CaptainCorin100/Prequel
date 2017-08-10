32c02b53bf22e6f3f25168ab4aed9405e3cb941d
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-10 12:10:19 +0100

    Colored Charts

--- static/DrawCharts.js
+++ static/DrawCharts.js
@@ -1,8 +1,8 @@
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-var users = [["National Average",6,"#ae2222","National Average"],["You",999,"#ed2211","You"]];
-var types = [["food", 50, "#32fd11"], ["luxuries", 22, "#659811"]];
+var users = [["National Average",6,"#43d1b2","National Average"],["You",999,"#a55221","You"]];
+var types = [["food", 50, "#21ed43"], ["luxuries", 22, "#659811"]];
 var count = 0;
 var values = [];
 
@@ -26,7 +26,7 @@ function drawChart () {
 	chart.draw (data, options);
 
 	var data2 = google.visualization.arrayToDataTable(arrayPersonal);
-	var options2={title:"Expenditures",height:"100%",width:"100%",backgroundColor:{fill:"transparent"}};
+	var options2={title:"Expenditures",height:"100%",width:"100%",backgroundColor:{fill:"transparent"},slices:{0:{color:"#21cd21"},1:{color:"#dddddd"},2:{color:"#eddfed"}}};
 	var chart2 = new google.visualization.PieChart (document.getElementById ("compare_left_chart"));
 	chart2.draw (data2, options2);
 }
