491f08403cf9f811ceb4897db1399e5605802c48
CaptainCorin100 <harrymcgregorwright@gmail.com>
2017-08-10 12:00:46 +0100

    Compare Stuff

--- static/DrawCharts.js
+++ static/DrawCharts.js
@@ -1,21 +1,27 @@
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
+var users = [["National Average",6,"#ae2222","National Average"],["You",999,"#ed2211","You"]];
+var types = [["food", 50, "#32fd11"], ["luxuries", 22, "#659811"]];
+var count = 0;
+var values = [];
+
+
+
+
 function drawChart () {
 	var arrayShared = [["Group", "spent", {role:"style"},{role:"annotation"}]];
-	var users = [["National Average",6,"#ae2222","National Average"],["You",999,"#ed2211","You"]];
 	for (var i = 0; i < users.length; i++) {
 		arrayShared.push (users[i]);
 	}
 
 	var arrayPersonal = [["Type", "bought", {role:"style"}]];
-	var types = [["food", 50, "#32fd11"], ["luxuries", 22, "#659811"]];
 	for (var j = 0; j < types.length; j++) {
 		arrayPersonal.push(types[j]);
 	}
 
 	var data = google.visualization.arrayToDataTable(arrayShared);
-	var options={title:"Expenditures",height:"100%",width:"100%",backgroundColor:{fill:"transparent"}};
+	var options={title:"Expenditures",height:"100%",width:"100%",backgroundColor:{fill:"transparent"},animation:{"startup":true,duration:1000,"easing":"out"}};
 	var chart = new google.visualization.BarChart (document.getElementById ("compare_right_chart"));
 	chart.draw (data, options);
 
